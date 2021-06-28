1523fec0ef2af63fdf46222a9b4b76e2cbf3935e
KentaMatsumura <photosynthesishope@gmail.com>
2021-06-28 23:02:22 +0900

    ヘッダーとフッターのテンプレート作成

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
+    <Header />
     <router-view/>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Header from './components/modules/Header'
+import Footer from './components/modules/Footer'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+    Footer
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import App from './App'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
